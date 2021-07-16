a71068c88be6ef5498bae924e303bed5632bf4a0
Collins Tatang <makungongcollins@gmail.com>
2021-07-16 20:35:09 +0100

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { addTask, editTask, clear } from './add_remove';
 
 let tasks = [
   {
-    index: 1, completed: false, description: 'Tax Collection'
+    index: 1, completed: false, description: 'Tax Collection',
   },
   {
-    index: 2, completed: true, description: 'Stand-up'
-  }
+    index: 2, completed: true, description: 'Stand-up',
+  },
 ];
 
 /**       Saves and retrieves from local storage       */
