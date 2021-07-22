50010cf2d315223638a2d13ef07b7a8e2e3a1aa7
Collins Tatang <makungongcollins@gmail.com>
2021-07-22 14:25:57 +0100

    add update to the index file

--- src/status.js
+++ src/status.js
@@ -1,13 +1,15 @@
+/* --- This file contains the functions needed to update the task object when a change is noted  */
+
 function updateTasks() {
-  const list = global.document.getElementsByTagName("li");
+  const list = document.getElementsByTagName('li');
   const tempTasks = [];
 
   if (list.length !== 0) {
     Array.from(list).forEach((li, index) => {
-      const div = li.getElementsByTagName("div")[0];
-      const completed = div.getElementsByTagName("input")[0].checked;
-      const id = div.getElementsByTagName("input")[0].name;
-      const description = div.getElementsByTagName("input")[1].value;
+      const div = li.getElementsByTagName('div')[0];
+      const completed = div.getElementsByTagName('input')[0].checked;
+      const id = div.getElementsByTagName('input')[0].name;
+      const description = div.getElementsByTagName('input')[1].value;
 
       const task = {
         completed,
@@ -18,7 +20,8 @@ function updateTasks() {
       tempTasks.push(task);
     });
   }
+
   return tempTasks;
 }
 
-module.exports = { updateTasks };
+export default updateTasks;
